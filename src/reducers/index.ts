184a0b5763dc628bc9915bcb1a35333d545fd6ea
kartikkalia74 <66248172+karthikkalia@users.noreply.github.com>
2021-07-29 23:43:06 +0530

    redux devtools enabled

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,6 @@
 
 
-import { combineReducers,createStore } from 'redux';
+import { createStore,combineReducers, applyMiddleware, compose } from 'redux';
 import  {article, initialState as articalState}  from '../reducers/article'
 import  {Question, initialState as questionState}  from '../reducers/question'
 import {Skill as skill ,initialState as skillState} from '../reducers/skills'
@@ -13,6 +13,17 @@ import {Skill as skill ,initialState as skillState} from '../reducers/skills'
 //         default:return initialstate;
 //     }
 // }
+
+ const composeEnhancers = typeof window === 'object' &&
+ (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+     // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+   }) : compose;
+//  const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+//  const store = createStore(reducer, /* preloadedState, */ compose(
+//     applyMiddleware(...middleware)
+//   ));
+
 export type reducerType = {
   article:typeof articalState,skill:typeof skillState,question:typeof questionState
 } 
@@ -35,4 +46,19 @@ export type RootAction = {}
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 //     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 //     : compose;
-export const Store  = createStore(reducers,{})
+// const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+// function logger({ getState }) {
+//   return _next => action => {
+//     console.log('will dispatch', action)
+
+//     // Call the next dispatch method in the middleware chain.
+//     const returnValue = _next(action)
+
+//     console.log('state after dispatch', getState())
+
+//     // This will likely be the action itself, unless
+//     // a middleware further in chain changed it.
+//     return returnValue
+//   }
+// }
+export const Store  = createStore(reducers,composeEnhancers(applyMiddleware()))
